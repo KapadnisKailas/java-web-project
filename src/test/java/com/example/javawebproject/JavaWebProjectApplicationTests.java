6764a609b289cd32630f0a520055d897efc23de0
Kapadnis, Kailas <kapadnis.kailas@gmail.com>
2020-01-21 09:35:51 -0800

    test case commented

--- src/test/java/com/example/javawebproject/JavaWebProjectApplicationTests.java
+++ src/test/java/com/example/javawebproject/JavaWebProjectApplicationTests.java
@@ -3,10 +3,10 @@ package com.example.javawebproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class JavaWebProjectApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
